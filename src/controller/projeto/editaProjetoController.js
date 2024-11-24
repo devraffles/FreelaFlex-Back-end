6ab2aced45b1ef6cea98483143bf21e9a31c3079
devraffles <devraffles@gmail.com>
2024-11-23 22:42:48 -0300

    ajuste do metodo async

--- src/controller/projeto/editaProjetoController.js
+++ src/controller/projeto/editaProjetoController.js
@@ -5,24 +5,31 @@ import registraCargoProjetoServices from "../../services/projeto/registraCargoPr
 export default async function editaProjetoController(req, res) {
     const { codprojeto, nome, descricao, habilidades, cargos } = req.body
 
-    const projeto = await editaProjetoServices(codprojeto, nome, descricao, habilidades);
+    const projeto = new Promise ((resolve, reject) => {
 
-    if(projeto.length === 0){
+        editaProjetoServices(codprojeto, nome, descricao, habilidades)
+
+    }).then((sucess) => {
+        deletaCargoProjetoServices(codprojeto).then((sucess) => {
+            cargos.forEach(cargo => {
+                registraCargoProjetoServices(cargo, codprojeto)
+            });
+
+            res.status(200).json({
+                codprojeto: projeto[0].codprojeto,
+                status: true,
+                message: "Registro Atualizado"
+            });
+        }).catch(() => {
+            res.status(422).json({
+                status: false,
+                message: "Erro ao excluir cargos do projeto"
+            });
+        });
+    }).catch(() => {
         res.status(422).json({
             status: false,
             message: "Erro ao editar projeto"
         });
-    }
-
-    await deletaCargoProjetoServices(codprojeto);
-
-    cargos.forEach(cargo => {
-        registraCargoProjetoServices(cargo, codprojeto)
-    });
-
-    res.status(200).json({
-        codprojeto: projeto[0].codprojeto,
-        status: true,
-        message: "Registro Atualizado"
     });
 }
