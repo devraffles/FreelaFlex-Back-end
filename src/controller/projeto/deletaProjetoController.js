0b31b21b1508a06d5f87b307545a24d7232472f3
devraffles <devraffles@gmail.com>
2024-11-25 13:27:26 -0300

    Arrumando Delete Projeto deletando proposta

--- src/controller/projeto/deletaProjetoController.js
+++ src/controller/projeto/deletaProjetoController.js
@@ -1,28 +1,36 @@
 import deletaProjetoServices from "../../services/projeto/deletaProjetoServices.js";
 import deletaCargoProjetoServices from "../../services/projeto/deletaCargoProjetoServices.js"
+import deletaPropostaServices from "../../services/proposta/deletaPropostaServices.js";
 
 export default async function deletaProjetoController(req, res) {
 
     const codProjeto = req.query.codProjeto
 
     const projeto = new Promise ((resolve, reject) => {
-        deletaCargoProjetoServices(codProjeto).then(() => {
-                deletaProjetoServices(codProjeto).then(() => {
-                    res.status(200).json({
-                        codprojeto: projeto[0].codprojeto,
-                        status: true,
-                        message: "Registro Excluído"
+        deletaPropostaServices("", codProjeto).then(() => {
+            deletaCargoProjetoServices(codProjeto).then(() => {
+                    deletaProjetoServices(codProjeto).then(() => {
+                        res.status(200).json({
+                            codprojeto: projeto[0].codprojeto,
+                            status: true,
+                            message: "Registro Excluído"
+                        });
+                    }).catch(() => {
+                        res.status(422).json({
+                            status: false,
+                            message: "Erro ao deletar projeto"
+                        });
                     });
-                }).catch(() => {
-                    res.status(422).json({
-                        status: false,
-                        message: "Erro ao deletar projeto"
-                    });
+            }).catch(() => {
+                res.status(422).json({
+                    status: false,
+                    message: "Erro ao deletar cargos do projeto e o projeto"
                 });
+            });
         }).catch(() => {
             res.status(422).json({
                 status: false,
-                message: "Erro ao deletar cargos do projeto e o projeto"
+                message: "Erro ao deletar proposta do projeto"
             });
         });
     });
--- src/services/proposta/deletaPropostaServices.js
+++ src/services/proposta/deletaPropostaServices.js
@@ -1,14 +1,16 @@
 import sql from "../../db/sql.js";
 import MyError from "../../error/myError.js";
 
-export default async function deletaPropostaServices(codProposta) {
+export default async function deletaPropostaServices(codProposta, codProjeto) {
 
     if(!codProposta){
         throw new MyError("Registro não encontrado", 404);
     }
 
     const proposta = await sql`
-        DELETE FROM proposta WHERE codproposta = ${codProposta} returning codproposta;
+        DELETE FROM proposta 
+        ${ codProposta !== "" ? sql`WHERE codproposta = ${codProposta}` : sql`WHERE codprojeto = ${codProjeto}` }
+        returning codproposta;
     `
 
     return proposta;
